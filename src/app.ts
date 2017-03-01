7211a7d788e04abdc74cac7152b5e9071b4e3663
Ya'ar Hever <yaarhever@gmail.com>
2017-03-01 22:21:09 +0100

    Use System.import

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,18 @@
 import * as ModuleA from "./async-modules/a";
 import * as ModuleB from "./async-modules/b";
 
+type Module =
+    typeof ModuleA |
+    typeof ModuleB;
+
 console.log('loading modules...');
-Promise.all([
-    new Promise<typeof ModuleA>((resolve, reject) => {
-        try {
-            require.ensure([], () => {
-                const moduleA = require('./async-modules/a') as typeof ModuleA;
-                console.log('module a loaded.');
-                resolve(moduleA);
-            }, "module-a");
-        }
-        catch (err) {
-            reject(err);
-        }
-    }),
-    new Promise<typeof ModuleA>((resolve, reject) => {
-        try {
-            require.ensure([], () => {
-                const moduleB = require('./async-modules/b') as typeof ModuleB;
-                console.log('module b loaded.');
-                resolve(moduleB);
-            }, "module-b");
-        }
-        catch (err) {
-            reject(err);
-        }
-    }),
-]).then(([moduleA, moduleB]) => {
-    console.log('modules loaded.');
-    moduleA.run();
-    moduleB.run();
-});
+Promise.all<Module>([
+    System.import('./async-modules/a'),
+    System.import('./async-modules/b')
+])
+    .then(modules => {
+        console.log('modules loaded.');
+        modules.forEach(m => m.run());
+    })
+    .catch(err => console.error(err));
 
